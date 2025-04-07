4da78c42720257744c439a10e525106c6c677017
sig2nya <minsikhong@hansol.com>
2025-04-07 13:13:26 +0900

    assign dynamic mem

--- modoo/ch4/assign.cpp
+++ modoo/ch4/assign.cpp
@@ -4,8 +4,10 @@
 class MyString {
 	char *string_content;
 	int string_length;
+	int memory_capacity;
 
 	public:
+
 		MyString(char c);
 		MyString(const char *str);
 		MyString(const MyString &str);
@@ -14,6 +16,12 @@ class MyString {
 		int length() const;
 		void print();
 		void println() const;
+
+		int capacity();
+		void reserve(int size);
+
+		MyString& assign(const MyString& str);
+		MyString& assign(const char *str);
 };
 
 MyString::MyString(char c) {
@@ -55,6 +63,60 @@ void MyString::println() const {
 		std::cout << string_content[i];
 	}
 	std::cout << std::endl;
+}
+
+MyString& MyString::assign(const MyString& str) {
+	if (str.string_length > memory_capacity) {
+		delete[] string_content;
+
+		string_content = new char[str.string_length];
+		memory_capacity = str.string_length;
+	}
+
+	for (int i = 0; i != str.string_length; i++) {
+		string_content[i] = str.string_content[i];
+	}
+
+	string_length = str.string_length;
+
+	return *this;
+}
+
+MyString& MyString::assign(const char* str) {
+	int str_length = strlen(str);
+	if (str_length > memory_capacity) {
+		delete[] string_content;
+
+		string_content = new char[str_length];
+		memory_capacity = str_length;
+	}
+
+	for (int i = 0; i != str_length; i++) {
+		string_content[i] = str[i];
+	}
+
+	string_length = str_length;
+
+	return *this;
+}
+
+int MyString::capacity() {
+	return memory_capacity;
+}
+
+void MyString::reserve(int size) {
+	if (size > memory_capacity) {
+		char *prev_string_content = string_content;
+
+		string_content = new char[size];
+		memory_capacity = size;
+
+		for (int i = 0; i != string_length; i++) {
+			string_content[i] = prev_string_content[i];
+		}
+
+		delete [] prev_string_content;
+	}
 }
 
 int main() {
