8f486c69106e38b07649fff2bc115dcf0420db54
홍민식 <hongminsik@hongminsig-ui-iMac.local>
2025-05-09 20:36:31 +0900

    crt dir

---
+++ stl/ch4/func_template/explicit_template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void Print(T a, T b) {
+	cout << a << ", " << b << endl;
+	return;
+}
+
+int main() {
+	Print<int>(10, 20);
+	Print<double>(0.123, 1.123);
+	Print<const char *>("ABC", "abcde");
+
+	return 0;
+}
