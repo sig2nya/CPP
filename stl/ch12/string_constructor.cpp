4e4d75e92858893b0d0c1ed5c0ed76cd0b6797ee
홍민식 <hongminsik@hongminsig-ui-iMac.local>
2025-05-09 20:18:43 +0900

    stl string

---
+++ stl/ch12/string_constructor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string t("Hello!"); // tmp string
+	const char* p1 = "Hello!";
+	const char* p2 = p1 + 6;
+
+	string s1;
+	string s2("Hello!");
+	string s3("Hello!", 2);
+	string s4(5, 'H');
+	string s5(t.begin(), t.end());
+	string s6(p1, p2);
+
+	// s:string object, sz : '\0' string, c : character, n : length, iter : iterator, p : pointer
+	cout << "s1() : " << s1 << endl;
+	cout << "s2(sz) : " << s2 << endl;
+	cout << "s3(sz, n) : " << s3 << endl;
+	cout << "s4(n, c) : " << s4 << endl;
+	cout << "s5(iter1, iter2) : " << s5 << endl;
+	cout << "s6(p1, p2) : " << s6 << endl;
+
+	return 0;
+}
